60059c79b238ec31d7ee0a7586b5fd6bdc030cd5
Alvaro Pinot <alvaro@neatnait.com>
2014-03-28 12:38:37 +0100

    added grunt-build-control

--- Gruntfile.js
+++ Gruntfile.js
@@ -348,6 +348,34 @@ module.exports = function (grunt) {
                 'imagemin',
                 'svgmin'
             ]
+        },
+         //automatic github pages branch updating
+        buildcontrol: {
+          options: {
+            dir: 'dist',
+            commit: true,
+            push: true,
+            message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+          },
+          pages: {
+            options: {
+              remote: 'https://github.com/NeatNait/zanin-landing.git',
+              branch: 'gh-pages'
+            }
+          },
+          //heroku: {
+          //  options: {
+          //    remote: 'git@heroku.com:example-heroku-webapp-1988.git',
+          //    branch: 'master',
+          //    tag: pkg.version
+          //  }
+          //},
+          local: {
+            options: {
+              remote: '../',
+              branch: 'build'
+            }
+          }
         }
     });
 
